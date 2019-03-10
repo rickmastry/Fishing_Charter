0fe906e024a95a5e51b8d63c600627b954d9d661
jah rik <jahrik@hotmail.com>
2019-03-10 12:47:30 -0400

    toggleopen javascriot

--- main.js
+++ main.js
@@ -5,16 +5,17 @@ panels.forEach(panel => panel.addEventListener('click', toggleOpen));
 panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
 
 function toggleOpen(){
+    setTimeout(window.onload = function () {
+    }, 3000);
     this.classList.toggle('open');
 };
 
 function toggleActive(e){
+    
     if(e.propertyName.includes('flex')){
     this.classList.toggle('open-active');
     }
     
-
-
 };
 
 
