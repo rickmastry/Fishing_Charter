1a5f1b99db6fb7d09896e83cc122ea6a4abb4f15
jah rik <jahrik@hotmail.com>
2019-03-10 14:02:32 -0400

    toggleopen false

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const panels = document.querySelectorAll('.panel');
 
-panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('click', toggleOpen, false));
 panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
 
 function toggleOpen(){
